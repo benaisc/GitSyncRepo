ae4d09720b1d1ede8ef8c3756bdf26d08be43f5c
guru <whatwhaaat@gmail.com>
2018-04-16 17:49:33 +0200

    create DB v2

--- Developpements/DNG_scripts/create_numpy.py
+++ Developpements/DNG_scripts/create_numpy.py
@@ -5,10 +5,13 @@ import matplotlib.image as mpimg
 import curses
 
 
-def create_db(images_dir_path, numpy_filename):
+def create_db(images_dir_path):
     # Return a list of directories contained in images_dir_path
     labels_list = []
     dataset = [] # need (num_classes, num_exemple, h, w, c)
+    file = open("db_labels.csv", "w")
+    file.write("label_id, label, number_images")
+    i=0
     for d in Path(images_dir_path).glob('*'):
         if not d.is_dir():
             continue;
@@ -19,14 +22,16 @@ def create_db(images_dir_path, numpy_filename):
 
         classDataSet = []
         for f in Path(d).glob('*.pgm'):
-            #print("file: {}".format(basename(str(f))))
             img = mpimg.imread(f) # img.shape = (256, 256)
-            img *= 255.0/img.max()
+            img = img.astype(np.float)
+            img /= 255.0
             img = np.reshape(img, newshape=(img.shape[0], img.shape[1], 1))
-            #print("img.shape: {}".format(img.shape))
             classDataSet.append(img) # list numpy images
 
+        file.write("{},{},{}\n".format(i, label, len(classDataSet)))
+        i += 1
         dataset.append(classDataSet)
+
     return np.array(dataset), labels_list
 
 def create_csv_labels(labelsList):
@@ -38,9 +43,8 @@ def create_csv_labels(labelsList):
         i += 1
     file.close()
 
-
 numpy_filename = 'lirmm_db.npy'
 train_dir = '/home/guru/STAGE/CODES/Developpements/DNG_scripts/TRAIN_DATA'
-data, labels = create_db(train_dir, numpy_filename)
+data, labels = create_db(train_dir)
 np.save(numpy_filename, data)
 create_csv_labels(labels)
---
+++ Developpements/DNG_scripts/dir_lister.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+from shutil import copy
+import os
+from random import shuffle
+
+def select_n_distribute(dir_path, dest_dir):
+    # Return a list of directories contained in images_dir_path
+    dir_list = [str(d) for d in Path(dir_path).glob('*') if d.is_dir()]
+    print("There is {} directories :".format(len(dir_list)))
+
+    for d in Path(dir_path).glob('*'):
+        if not d.is_dir():
+            continue;
+
+        label = str(d).split('/')[-1]
+        file_list = [str(f) for f in Path(d).glob('*.pgm')]
+        if len(file_list) > 500:
+            print("{} : {}".format(label, len(file_list)))
+            os.makedirs(dest_dir+'/'+label)
+            print("Shuffling...")
+            shuffle(file_list)
+            print("Copying...")
+            for i in range(590):
+                copy(file_list[i], str(dest_dir)+'/'+label)
+
+
+train_dir = '/home/guru/STAGE/CODES/Developpements/DNG_scripts/TRAIN_DATA'
+dest_dir = '/home/guru/STAGE/CODES/Developpements/DNG_scripts/LIRMMv0'
+
+select_n_distribute(train_dir, dest_dir)
