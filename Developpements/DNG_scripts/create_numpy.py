29e6d7117fa6ab03c65d33925b27b7bc3717a90a
guru <whatwhaaat@gmail.com>
2018-04-24 10:08:44 +0200

    numpy binary reviewed

--- Developpements/DNG_scripts/create_numpy.py
+++ Developpements/DNG_scripts/create_numpy.py
@@ -11,13 +11,16 @@ in the form: (num_classes, num_exemple, h, w, c)
 """
 
 def create_db(images_dir_path):
-    # Return a list of directories contained in images_dir_path
+    if not Path(train_dir).is_dir():
+        print('Error')
+        exit()
+
     csvInfo = []
     dataset = [] # need (num_classes, num_exemple, h, w, c)
     file = open("lirmm_128_labels.csv", "w")
     file.write("label_id, label, number_images\n")
-    i=0
-    for d in Path(images_dir_path).glob('*'):
+    for d in Path(images_dir_path).glob('*'): # for files/dir in pathdir
+        # skipping files
         if not d.is_dir():
             continue;
         # Extract info about image dir path
@@ -32,22 +35,17 @@ def create_db(images_dir_path):
             img = np.reshape(img, newshape=(img.shape[0], img.shape[1], 1))
             classDataSet.append(img) # list numpy images
 
-        csvInfo.append([i, label, len(classDataSet)])
+        csvInfo.append([label, len(classDataSet)])
         dataset.append(classDataSet)
-        i += 1
 
     dataset.sort(key=len)
     csvInfo.sort(key=lambda item: item[2])
     for i in range(len(csvInfo)):
-        file.write("{},{},{}\n".format(csvInfo[i][0], csvInfo[i][1], csvInfo[i][2]))
+        file.write("{},{},{}\n".format(i, csvInfo[i][0], csvInfo[i][1]))
 
     return np.array(dataset)
 
 
-
 train_dir = '/media/icar/269f599f-6a72-48fd-b97c-941595d7b39f/Charles/128/128_TRAIN_DATA'
-if not Path(train_dir).is_dir():
-    print('Error')
-    exit()
 data = create_db(train_dir)
 np.save('lirmm_128.npy', data)
